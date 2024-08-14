884adf05d6f89f60de13a769c7a7bb879afd13e1
Alvin So <alvinso@kbquest.com>
2024-08-14 17:04:57 +0800

    add min max args

--- main.go
+++ main.go
@@ -2,14 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"prime/constants"
 	"prime/finder"
+	"strconv"
 	"time"
 )
 
 func main() {
 	var min int = 1
 	var max int = constants.Giga
+
+	args := os.Args
+	if len(args) == 3 {
+		minArg := args[1]
+		maxArg := args[2]
+
+		if x, err := strconv.Atoi(minArg); err == nil {
+			min = x
+		} else {
+			fmt.Println("Invalid min argument")
+			return
+		}
+
+		if y, err := strconv.Atoi(maxArg); err == nil {
+			max = y
+		} else {
+			fmt.Println("Invalid max argument")
+			return
+		}
+
+		isValidMinMax := validateMinMax(min, max)
+		if !isValidMinMax {
+			return
+		}
+	}
+
 	fmt.Printf("Finding prime numbers from %d to %d\n", min, max)
 
 	startTime := time.Now()
@@ -28,4 +56,28 @@ func main() {
 	fmt.Printf("Execution time: %v\n", endTime.Sub(startTime))
 	fmt.Printf("Number of prime numbers: %d\n", len(primeNumbers))
 	fmt.Printf("Number found per second: %.2f\n", numberPerSecond)
+}
+
+func validateMinMax(min int, max int) bool {
+	if min < 1 {
+		fmt.Println("Min cannot be less than 1")
+		return false
+	}
+
+	if max < 1 {
+		fmt.Println("Max cannot be less than 1")
+		return false
+	}
+
+	if min == max {
+		fmt.Println("Min and max cannot be the same")
+		return false
+	}
+
+	if min > max {
+		fmt.Println("Min cannot be greater than max")
+		return false
+	}
+
+	return true
 }
